2c61e8e1703adfb34387b5d686004cb9430d9713
Iñaki Esquivel <90475192+inaki321@users.noreply.github.com>
2025-02-28 12:33:54 -0600

    Create variables.tf

---
+++ SQLAZ/tf/variables.tf
@@ -0,0 +1,26 @@
+variable "appName" {
+  description = "The name of my sql application"
+  type        = string
+}
+
+variable "location" {
+  description = "Location of the AZ resource"
+  type        = string
+}
+
+variable "environment" {
+  description = "Application environment (DEV, Non-prod, prod etc.)"
+  type        = string
+}
+
+variable "databaseServer" {
+  description = "The hosted zone for the domain"
+  type        = string
+}
+
+variable "databaseServer" {
+  description = "Configuration for the database server"
+  type = object({
+    name     = string
+  })
+}
